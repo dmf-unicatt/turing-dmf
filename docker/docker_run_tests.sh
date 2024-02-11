27662d2aa24c46560fde11a9b5b5df9e7a058441
Francesco Ballarin <francesco.ballarin@unicatt.it>
2024-02-11 10:35:56 +0100

    Simplify docker_run_tests bash script: will add back mathrace interaction tests afterwards

--- docker/docker_run_tests.sh
+++ docker/docker_run_tests.sh
@@ -10,27 +10,8 @@ set -e
 CONTAINER_ID_FILE=".docker_container_id"
 CONTAINER_ID=$(cat "${CONTAINER_ID_FILE}")
 
-MATHRACE_COMPATIBILITY_DIR=$(realpath ../mathrace_compatibility)
-TURING_DIR==$(realpath ../turing)
-
-cd ${MATHRACE_COMPATIBILITY_DIR}/scripts
-python3 mathrace_log_to_turing_json.py
-
-cd ${MATHRACE_COMPATIBILITY_DIR}/tests/data
-rm -rf journal_2020.converted.json journal_2021.converted.json journal_2022.converted.json
-for JSON_FILE in *.converted.json; do
-    docker cp ${JSON_FILE} ${CONTAINER_ID}:/root/turing/engine/test_gare/${JSON_FILE/.converted.json/.json}
-done
-for SCORE_FILE in *.expected.score; do
-    docker cp ${SCORE_FILE} ${CONTAINER_ID}:/root/turing/engine/test_gare/${SCORE_FILE/.expected.score/.score}
-done
-
 docker exec ${CONTAINER_ID} /bin/bash -c "\
     export DISPLAY=$DISPLAY && \
     cd turing && \
-    echo 'Run disfida tests first' && \
-    python3 manage.py test engine.tests.LiveTests.test_json_folder && \
-    rm -rf engine/test_gare/*.json engine/test_gare/*.score && \
-    echo 'Run all remaining tests afterwards' && \
     python3 manage.py test \
 "
