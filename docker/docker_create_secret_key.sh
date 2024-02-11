b3747278c23fb84e34ac926ea01ea1101908611d
Francesco Ballarin <francesco.ballarin@unicatt.it>
2024-02-11 16:48:07 +0100

    Remove % from the list of available characters when generating the secret key, since it requires to be escaped

--- docker/docker_create_secret_key.sh
+++ docker/docker_create_secret_key.sh
@@ -13,6 +13,6 @@ if [[ -f "${SECRET_KEY_FILE}" ]]; then
     echo "If you want to destroy it and create a new one, please remove the ${SECRET_KEY_FILE} file"
     exit 1
 else
-    SECRET_KEY=$(cat /dev/urandom | tr -dc 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)' | head -c 50; echo)
+    SECRET_KEY=$(cat /dev/urandom | tr -dc 'abcdefghijklmnopqrstuvwxyz0123456789!@#$^&*(-_=+)' | head -c 50; echo)
     echo ${SECRET_KEY} > ${SECRET_KEY_FILE}
 fi
