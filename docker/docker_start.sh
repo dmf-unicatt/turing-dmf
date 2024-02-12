9c5040ee1d66e133d3260196c03de01674a3eee6
Francesco Ballarin <francesco.ballarin@unicatt.it>
2024-02-12 12:26:14 +0100

    Improve logging in docker start script

--- docker/docker_start.sh
+++ docker/docker_start.sh
@@ -14,8 +14,23 @@ if [ "$( docker container inspect -f '{{.State.Running}}' ${CONTAINER_ID} )" == 
     echo "If you want to attach a terminal to it, please run docker_terminal.sh"
     exit 1
 else
+    START_TIME=$(date +%s)
     docker start ${CONTAINER_ID}
     # Entrypoint outputs are not reported to stdout, but logged: fetch them and print them to stdout, after waiting
     # a reasonable amount of time for the entrypoint to be done
-    sleep 5 && docker logs --since "6s" --timestamps ${CONTAINER_ID}
+    MAX_TRY=10
+    for TRY in $(seq 1 ${MAX_TRY}); do
+        DOCKER_LOG=$(docker logs --since "${START_TIME}" --timestamps ${CONTAINER_ID})
+        if [[ "${DOCKER_LOG}" == *"Starting the server"* ]]; then
+            echo "${DOCKER_LOG}"
+            break
+        else
+            if [[ ${TRY} != ${MAX_TRY} ]]; then
+                sleep 1
+            else
+                echo "${DOCKER_LOG}"
+                echo "Server did not start after ${MAX_TRY} seconds. Log output may be incomplete."
+            fi
+        fi
+    done
 fi
