6d70a94a7cb2728332501ff9475c638750bfc68c
Francesco Ballarin <francesco.ballarin@unicatt.it>
2024-02-14 17:38:23 +0100

    Add empty performance tests

---
+++ mathrace_interaction/tests/performance/test_increasing_load.py
@@ -0,0 +1,15 @@
+# Copyright (C) 2024 by the Turing @ DMF authors
+#
+# This file is part of Turing @ DMF.
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+"""Test the application with an increasing load."""
+
+def test_public_views_increasing_load() -> None:
+    """Test public classification views with increasing load."""
+    pass
+
+
+def test_private_views_increasing_load() -> None:
+    """Test private insertion views with increasing load."""
+    pass
