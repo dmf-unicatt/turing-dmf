661d5e744ed56897d98ec162b1b0e8965317d495
Francesco Ballarin <francesco.ballarin@unicatt.it>
2025-12-23 12:30:50 +0100

    Use headless=new mode instead of the legacy headless mode

--- mathrace_interaction/mathrace_interaction/network/turing_classification_selenium.py
+++ mathrace_interaction/mathrace_interaction/network/turing_classification_selenium.py
@@ -63,7 +63,7 @@ class TuringClassificationSelenium:
         options = selenium.webdriver.ChromeOptions()
         options.add_argument("--no-sandbox")
         options.add_argument("--window-size=1920,1080")
-        options.add_argument("--headless")
+        options.add_argument("--headless=new")
         options.add_argument("--disable-gpu")
         options.add_argument("--disable-dev-shm-usage")
         self._browser = selenium.webdriver.Chrome(service=service, options=options)
